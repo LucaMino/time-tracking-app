ef0d8364e6cbcaca89e4c70509143d1baa4b2939
Luca Minorello <luca.minorello@makingscience.com>
2024-02-06 14:54:25 +0100

    feat: init spring project

---
+++ src/main/java/com/example/timetrackingapp/TimeTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.timetrackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/timetrackingapp/TimeTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timetrackingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
